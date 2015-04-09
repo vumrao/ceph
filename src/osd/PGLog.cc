3f8d075e24af560c3e776a10441820c5d160e766
David Zafman <dzafman@redhat.com>
2015-04-09 16:26:07 -0700

    osd: Show number of divergent_priors in log message
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit 792590925db22cb6cb54e9e3332dd21cbf6f9186)

--- src/osd/PGLog.cc
+++ src/osd/PGLog.cc
@@ -717,7 +717,7 @@ void PGLog::write_log(
     dout(10) << "write_log with: "
 	     << "dirty_to: " << dirty_to
 	     << ", dirty_from: " << dirty_from
-	     << ", dirty_divergent_priors: " << dirty_divergent_priors
+	     << ", divergent_priors: " << divergent_priors.size()
 	     << ", writeout_from: " << writeout_from
 	     << ", trimmed: " << trimmed
 	     << dendl;
